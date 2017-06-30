ad0ac1d9a72609d131baefcbfd73d389fb32b893
Christian Parkinson <cparkinson@invoca.com>
2017-06-30 12:27:49 -0400

    Extracts OIDC discovery into its own class

--- lib/kubeclient/oidc_token.rb
+++ lib/kubeclient/oidc_token.rb
@@ -1,14 +1,54 @@
 require 'json/jwt'
+require 'delegate'
 
 module Kubeclient
   OidcException = Class.new(StandardError)
 
-  class OidcToken
-    attr_reader :client_id, :refresh_token, :client_secret, :idp
+  class OidcDiscovery
+    attr_reader :idp
 
     DISCOVERY_PATH = '.well-known/openid-configuration'.freeze
-    JWKS_FIELD = 'jwks_uri'.freeze
-    TOKEN_ENDPOINT_FIELD = 'token_endpoint'.freeze
+
+    FIELD_MAP = {
+      token_endpoint: 'token_endpoint',
+      jwks_endpoint: 'jwks_uri'
+    }
+
+    class << self
+      def fields
+        FIELD_MAP.keys
+      end
+    end
+
+    FIELD_MAP.each do |method, field|
+      define_method method do
+        validate_and_retrieve_field(field)
+      end
+    end
+
+    def initialize(idp)
+      @idp = idp
+    end
+
+    private
+
+    def discovered
+      @discovered ||= JSON.parse(RestClient.get(discovery_url))
+    end
+
+    def discovery_url
+      "#{idp.chomp('/')}/#{DISCOVERY_PATH}"
+    end
+
+    def validate_and_retrieve_field(field)
+      discovered.key?(field) && discovered[field] or raise OidcException,
+        "OIDC discovery URL #{discovery_url} did not return a JSON document containing a '#{field}' field"
+    end
+  end
+
+  class OidcToken
+    attr_reader :client_id, :refresh_token, :client_secret, :idp, :oidc_discovery
+    delegate *OidcDiscovery.fields, to: :oidc_discovery
 
     REFRESH_WITHIN = 300
     ID_TOKEN = 'id_token'.freeze
@@ -21,6 +61,7 @@ module Kubeclient
       @idp = idp
       @id_token = id_token
       @refresh_token = refresh_token
+      @oidc_discovery = OidcDiscovery.new(idp)
 
       decode_token_payload(ignore_kid_not_found: true)
     end
@@ -66,27 +107,6 @@ module Kubeclient
 
     def jwks
       JSON::JWK::Set.new(JSON.parse(RestClient.get(jwks_endpoint)))
-    end
-
-    def token_endpoint
-      @token_endpoint ||= oidc_discovery_field(TOKEN_ENDPOINT_FIELD)
-    end
-
-    def jwks_endpoint
-      @jwks_endpoint ||= oidc_discovery_field(JWKS_FIELD)
-    end
-
-    def oidc_discovery
-      @oidc_discovery ||= JSON.parse(RestClient.get(oidc_discovery_url))
-    end
-
-    def oidc_discovery_field(field)
-      oidc_discovery.key?(field) && oidc_discovery[field] or raise OidcException,
-        "OIDC discovery URL #{oidc_discovery_url} did not return a JSON document containing a '#{field}' field"
-    end
-
-    def oidc_discovery_url
-      @idp_discovery_url ||= "#{idp.chomp('/')}/#{DISCOVERY_PATH}"
     end
   end
 end
